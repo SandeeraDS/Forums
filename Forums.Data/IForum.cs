4079ce156a64c9f64685b68ae992428b8b3ef27f
Dilusha Sandeera Jayampathi <32511955+SandeeraDS@users.noreply.github.com>
2019-02-09 19:01:30 +0530

    add service layer 2

---
+++ Forums.Data/IForum.cs
@@ -0,0 +1,21 @@
+﻿using Forums.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forums.Data
+{
+    public interface IForum
+    {
+        Forum GetByid(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTilte(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+
+    }
+}
---
+++ Forums/Controllers/ForumController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forums.Data;
+using Forums.Data.Models;
+using Forums.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forums.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll().Select(forum => new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                Description = forum.Description
+            });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+            return View(model);
+        }
+    }
+}
